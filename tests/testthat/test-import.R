ecafd11645f7658b03bb84b230b6b9a216d484ad
Robert A. Amezquita <2438701+robertamezquita@users.noreply.github.com>
2019-01-18 09:56:22 -0800

    initial gmt import function

---
+++ tests/testthat/test-import.R
@@ -0,0 +1,20 @@
+
+good_gmt <- system.file(package = "unisets", "extdata", "example.gmt")
+dupe_ele <- system.file(package = "unisets", "extdata", "test_duplicated-elements.gmt")
+dupe_set <- system.file(package = "unisets", "extdata", "test_duplicated-sets.gmt")
+
+test_that("GMT importing produces valid BaseSets.", {
+    good <- importGMT(good_gmt)
+    set_len <- as.numeric(setLengths(good))
+    expect_equal(set_len, c(74, 200, 200, 200))
+})
+
+test_that("GMT importing produces error with duplicated set names.", {
+    expect_error(importGMT(dupe_set))
+})
+
+test_that("GMT importing passes with duplicated elements within set.", {
+    pass <- importGMT(dupe_ele)
+    set_len <- as.numeric(setLengths(pass))
+    expect_equal(set_len, c(3, 1))
+})
