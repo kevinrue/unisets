a883cb60f1341e0d1bb6a235b5068de401416915
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2019-01-30 19:52:25 +0000

    quickfix for importing GMT files following rtracklayer generics

--- R/import.r
+++ R/import.r
@@ -1,22 +1,47 @@
+
+## extending RTLFile class to GMT ----
+
+#' GMTFile Class
+#'
+#' @rdname import
+#'
+#' @export
+#' @importClassesFrom rtracklayer RTLFile
+setClass("GMTFile", contains = "RTLFile")
+
+#' @param resource For `GMTFile()`, the .gmt file that will be imported in.
+#'
+#' @rdname import
+#'
+#' @return For `GMTFile()`, an object representing the path to a .gmt file on disk
+#'
+#' @export
+GMTFile <- function(resource) {
+    new("GMTFile", resource = resource)
+}
+
 ## credit to Michael Lawrence
 ## implementation is largely based on:
 ## https://github.com/lawremi/rtracklayer/blob/master/R/bed.R
 
-## extending RTLFile class to GMT ----
-#' @importClassesFrom rtracklayer RTLFile
 setGeneric("import.gmt", function(con, ...) standardGeneric("import.gmt"))
 
 ## import() ----
-#' @importFrom rtracklayer import
+
+#' @importMethodsFrom rtracklayer import
+#' @importFrom rtracklayer FileForFormat
 setMethod("import.gmt", "ANY", function(con, ...) {
-    import(con, format = "gmt", ...)
+    x <- FileForFormat(con)
+    print(x)
+    # print(file.exists(resource(x)))
+    import(x, ...)
 })
 
-#' @importFrom rtracklayer import 
+#' @importFrom rtracklayer resource
 #' @importFrom S4Vectors DataFrame
 #' @importFrom utils stack
 setMethod("import", "GMTFile", function(con, format, text...) {
-    ## import.gmt <- function(path) 
+    print("import GMTFile")
     ## Read in GMT into a list format
     path <- resource(con)
     sets <- readLines(path)
@@ -29,7 +54,7 @@ setMethod("import", "GMTFile", function(con, format, text...) {
     if (length(unique(names)) != length(names)) {
         dups <- names[duplicated(names)]
         err <- paste0("Duplicated geneset set names exist for the sets below. ",
-            "Please check your GMT file.\n\n", 
+            "Please check your GMT file.\n\n",
             c(paste0(dups, collapse = '\n')), '\n\n')
         stop(err)
     }
@@ -49,5 +74,44 @@ setMethod("import", "GMTFile", function(con, format, text...) {
     bs <- BaseSets(map, setData = set_data)
     return(bs)
 })
-              
-   
+
+
+## export() ----
+
+#' @importClassesFrom rtracklayer RTLFile
+setGeneric("export.gmt", function(object, con, ...) standardGeneric("export.gmt"))
+
+#' @importFrom rtracklayer export
+setMethod("export.gmt", "ANY", function(object, con, ...) {
+    export(object, con, "gmt", ...)
+})
+
+#' @importFrom rtracklayer export
+#' @importFrom utils write.table
+setMethod("export", c("BaseSets", "GMTFile"), function(object, con, format, ...) {
+    x <- object
+    path <- resource(con)
+    if (!"source" %in% colnames(elementMetadata(setData(x)))) {
+        message("'source' column not found in elementMetadata(setData(x)), setting to NA for export")
+        source <- DataFrame(source = rep(NA_character_, nSets(x)),
+                            row.names = id(setData(x)))
+    } else {
+        source <- DataFrame(source = elementMetadata(setData(x))[["source"]],
+                            row.names = id(setData(x)))
+    }
+
+    ## Collapse into tab separated list
+    df <- data.frame(relations(x))
+    df$source <- source[df$set, ]
+    df <- df[order(df$set, df$element), ]
+    set_list <- lapply(with(df, split(df, set)), function(x) {
+        paste(x$set[1], x$source[1],
+              paste(x$element, collapse = "\t"),
+              sep = "\t")
+    })
+
+    ## Collapse each set list into a row and write out
+    out <- paste(unlist(set_list), collapse = "\n")
+    write.table(out, path, sep = "\t",
+                col.names = FALSE, row.names = FALSE, quote = FALSE)
+})
